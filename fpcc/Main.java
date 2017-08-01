9498a919bf8ec1c7bf4da4b1962d1e93a164f437
Aislan Jefferson <aislanjsb@gmail.com>
2017-08-01 11:38:45 -0300

    Modificado o webroot para o diretorio raiz do executavel, inserido opcao de escolha da porta que sera escutada

--- fpcc/Main.java
+++ fpcc/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Server mode");
 		int port = 8080;
+		if (args.length == 1)
+			port = Integer.parseInt(args[0]);
 		Server s = new Server(port);
 		System.out.println("Executando na porta " + port);
 		s.run();
