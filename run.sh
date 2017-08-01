786edaf1854d4d32ede2539b44c4eecdcb1ceb18
Aislan Jefferson <aislanjsb@gmail.com>
2017-08-01 13:11:41 -0300

    Adicionado scripts de build e de start/stop

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+case "$1" in
+		start)
+            nohup java -jar server.jar $2 > server.log 2>&1 &
+			echo $! > .server.pid
+			echo "Servidor iniciado"
+            ;;
+         
+        stop)
+            if [ -e ".server.pid" ]; then
+				kill -9 `cat .server.pid`
+			rm .server.pid
+			echo "Servidor encerrado!"
+			fi 
+            ;;
+        *)
+            echo $"Uso: $0 {start|stop}"
+            exit 1
+ 
+esac
